41e6d8791f45bd3ae2be391eb45085e17180a939
Saikat Roy <njmsaikat@gmail.com>
2020-06-09 02:17:56 +0600

    only background working

---
+++ script.js
@@ -0,0 +1,15 @@
+function set(){
+    let elements = document.getElementsByName('cssProperty');
+    for( let i = 0; i < elements.length;i++)
+    {
+        let cssProperty = elements[i].getAttribute('id');
+        let cssValue = elements[i].value;
+
+        let div = document.getElementById('modify');
+        div.style[cssProperty] = cssValue;
+
+        
+    }
+}
+
+document.getElementById('set').addEventListener('click',set);
