21be103c003404f58cfe790fe533c95320aac7f0
Saikat Roy <njmsaikat@gmail.com>
2020-06-09 11:03:36 +0600

    working but still have to modify

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function set(){
     let elements = document.getElementsByName('cssProperty');
+    
     for( let i = 0; i < elements.length;i++)
     {
         let cssProperty = elements[i].getAttribute('id');
         let cssValue = elements[i].value;
-
+    
         let div = document.getElementById('modify');
         div.style[cssProperty] = cssValue;
 
+        let p = document.getElementById('text');
+        p.style[cssProperty]=cssValue;
         
     }
 }
